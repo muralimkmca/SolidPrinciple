602b1c991fef7854762ca93dc3f1d7aa1827259d
Murali <muralimk@outlook.com>
2022-09-05 17:37:59 +0530

    Interface segregation principle

---
+++ InterfaceSegregationprinciple/IMessage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationprinciple
+{
+    public interface IMessage
+    {
+        string ToAddress { get; set; }
+
+        string BccAddress { get; set; }
+
+        string MessageBody { get; set; }
+
+        string Subject { get; set; }
+
+        bool send();
+    }
+
+    public class smtpmessage : IMessage
+    {
+        public string ToAddress { get; set; }
+        public string BccAddress { get; set; }
+        public string MessageBody { get; set; }
+        public string Subject { get; set; }
+
+        public bool send()
+        {
+            //Write logic for send the mail
+            return true;
+        }
+    }
+
+
+    public class smsmessage : IMessage
+    {
+        public string ToAddress { get; set; }
+        public string BccAddress { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string MessageBody { get; set; }
+        public string Subject { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool send()
+        {
+            //send the sms logic
+            return true;
+        }
+    }
+}
---
+++ InterfaceSegregationprinciple/InterfaceSegrationPrinciple.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationprinciple
+{
+    public interface ISPMessage
+    {
+        bool Send(string ToAddress, string MessageBody);
+    }
+
+    public interface IEmailMsg : ISPMessage
+    {
+        string Subject { get; set; }
+
+        string BCCAddress { get; set; }
+    }
+
+    public class smtpmsg : IEmailMsg
+    {
+        public string Subject { get; set; }
+        public string BCCAddress { get; set; }
+
+        public bool Send(string ToAddress, string MessageBody)
+        {
+            // mail send logic
+            return true;
+        }
+    }
+
+
+    public class smsmsg : ISPMessage
+    {
+        public bool Send(string ToAddress, string MessageBody)
+        {
+            //sms send logic
+
+            return true;
+        }
+    }
+
+}
